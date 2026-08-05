74ab0cbdc4cf9eead9e23ad85779f6fed04c1e3d
Daniel Mathisen Breivik <danielmbreivik@hotmail.com>
2026-08-05 13:17:25 +0200

    feat: wrap minified code in an async IIFE and update build process

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const { minifyTemplates, writeFiles } = require('esbuild-minify-templates');
-require('esbuild').build({
+
+(async () => {
+await require('esbuild').build({
   entryPoints: ['acos.js'],
   bundle: true,
   target: 'es2022',
@@ -13,3 +16,11 @@ require('esbuild').build({
     ],
 })
 
+const code = fs.readFileSync('acos.min.js', 'utf8');
+const wrappedCode = `(async function() {${code
+    // dont question this regex, it works as long as the built code doesn't add any newlines in the middle of the code
+    .replace(/\n/g, ' ')
+}})();`;
+fs.writeFileSync('acos.min.js', wrappedCode, 'utf8');
+
+})();
